646fd5053fcb458ae8ff1d64e3d93c6f5ff86921
Changwhi <whinada@gmail.com>
2023-10-13 13:15:30 -0700

    Feat: Installs express and ejs, Implements index.js page

---
+++ index.js
@@ -0,0 +1,23 @@
+global.base_dir = __dirname;
+global.abs_path = function(path) {
+  return base_dir + path
+}
+golobal.include = function(file) {
+  return require(abs_path('/' + file));
+}
+
+const express = require('express');
+const router = include('routes/router')
+
+const port = 3000;
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(__dirname + "/public"));
+app.use('/', router);
+
+app.listen(port, () => {
+  console.log("node app listening on port: " + port);
+});
