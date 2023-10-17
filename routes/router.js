0858840155b46d1fe2c497e4d88955a72c3fe0c3
Changwhi <whinada@gmail.com>
2023-10-16 21:19:24 -0700

    Fix: login and sign up functions

--- routes/router.js
+++ routes/router.js
@@ -7,6 +7,7 @@ require("dotenv").config();
 
 
 
+const db_users = include('database/users');
 
 
 const saltRounds = 12;
@@ -16,6 +17,13 @@ const mongodb_user = process.env.MONGODB_USER;
 const mongodb_password = process.env.MONGODB_PASSWORD;
 const mongodb_session_secret = process.env.MONGODB_SESSION_SECRET;
 const node_session_secret = process.env.NODE_SESSION_SECRET;
+
+
+const passwordSchema = Joi.object({
+  password: Joi.string().pattern(/(?=.*[a-z])/).pattern(/(?=.*[A-Z])/).pattern(/(?=.*[!@#$%^&*])/).pattern(/(?=.*[0-9])/).min(12).max(50).required()
+});
+const mongoSanitize = require("express-mongo-sanitize");
+
 
 var mongoStore = MongoStore.create({
   mongoUrl: `mongodb+srv://${mongodb_user}:${mongodb_password}@cluster1.5f9ckjd.mongodb.net/COMP4921_Project2_DB?retryWrites=true&w=majority`,
@@ -32,11 +40,32 @@ router.use(
     resave: true,
   })
 );
+function isValidSession(req) {
+  console.log("isValidSession")
+  if (req.session.authenticated) {
+    return true;
+  }
+  return false;
+}
 
+function sessionValidation(req, res, next) {
+  console.log("hit sessionValidation")
+  if (!isValidSession(req)) {
+    res.locals.isLoggedIn = req.session.authenticated === true;
+    req.session.destroy();
+    res.redirect('/login');
+    return;
+  }
+  else {
+    res.locals.isLoggedIn = req.session.authenticated === true;
+    next();
+  }
+}
 
 router.get("/", (req, res) => {
   console.log("idex page hit")
-  res.render("index", { isLoggedIn: false })
+  const isLoggedIn = isValidSession(req)
+  res.render("index", { isLoggedIn: isLoggedIn })
 })
 
 
@@ -51,13 +80,13 @@ router.get("/login", async (req, res) => {
 
 router.get('/logout', (req, res) => {
   console.log("Logging out");
-  
+
   req.session.destroy(err => {
     if (err) {
       console.error('Error destroying session:', err);
       return res.status(500).send('Failed to log out');
     }
-    
+
     res.redirect('/login');
   });
 });
@@ -69,14 +98,12 @@ router.get("/signup", async (req, res) => {
 
 });
 
-
 router.post("/loggingin", async (req, res) => {
-  const passwordSchema = Joi.string().pattern(new RegExp('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*]).{8,}$')).required();
   var email = req.body.email;
   var password = req.body.password;
   var users = await db_users.getUsers();
   let user;
-  
+
   for (let i = 0; i < users.length; i++) {
     if (users[i].email == email) {
       user = users[i];
@@ -87,13 +114,13 @@ router.post("/loggingin", async (req, res) => {
   if (user === undefined) {
     res.render('login', { message: "Why did you enter the wrong email?!", isLoggedIn: false });
     return;
-  } 
+  }
 
   const validationResult = passwordSchema.validate(password);
 
   if (validationResult.error) {
     let errorMsg = validationResult.error.details[0].message;
-    
+
     if (errorMsg.includes("(?=.*[a-z])")) {
       errorMsg = "Password must have at least 1 lowercase.";
     } else if (errorMsg.includes("(?=.*[A-Z])")) {
@@ -106,7 +133,7 @@ router.post("/loggingin", async (req, res) => {
       errorMsg = null;
     }
 
-    res.render("login", { message: errorMsg, isLoggedIn: false });
+    res.render("error", { message: errorMsg, isLoggedIn: false });
     return;
   }
 
@@ -132,12 +159,12 @@ router.post("/submitUser", async (req, res) => {
   var password = req.body.password;
   var name = req.body.name;
   var hashedPassword = bcrypt.hashSync(password, saltRounds);
-  
+
   const validationResult = passwordSchema.validate({ password });
 
   if (validationResult.error) {
     let errorMsg = validationResult.error.details[0].message;
-    
+
     if (errorMsg.includes("(?=.*[a-z])")) {
       errorMsg = "Password must have at least 1 lowercase.";
     } else if (errorMsg.includes("(?=.*[A-Z])")) {
@@ -153,7 +180,7 @@ router.post("/submitUser", async (req, res) => {
     var success = await db_users.createUser({ email: email, hashedPassword: hashedPassword, name: name });
 
     if (success) {
-      res.redirect('/login');
+      res.redirect('/');
       return;
     } else {
       res.render('error', { message: `Failed to create the user ${email}, ${name}`, title: "User creation failed" });
@@ -162,17 +189,12 @@ router.post("/submitUser", async (req, res) => {
 });
 
 router.get('/profile', (req, res) => {
-  res.render('profile', {message: "Profile", isLoggedIn: false})
+  res.render('profile', { message: "Profile", isLoggedIn: false })
 })
 
 
-function isValidSession(req) {
-  console.log("isValidSession")
-  if (req.session.authenticated) {
-    return true;
-  }
-  return false;
-}
+
+
 
 module.exports = router;
 
